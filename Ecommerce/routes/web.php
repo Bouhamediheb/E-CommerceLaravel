567d610bdd0bc84843958239523cc8cca23925ea
Iheb Bouhamed <ihebbouhamediit@gmail.com>
2023-10-23 15:22:16 +0100

    Working VueJS starter code

--- Ecommerce/routes/web.php
+++ Ecommerce/routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/{any}', function () {
+    return view('layout.app');
+    })->where('any', '.*');
+    
+Route::get('/accueil', function () {
+        return view('layout.app');
+        })->where('any', '.*');
+    
